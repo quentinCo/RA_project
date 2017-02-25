ec0e264c4d64421520324226ebd04779c2f1c0a3
quentinCo <quentin.couillard@gmail.com>
2017-02-25 20:12:54 +0100

    *add: MusicLoader

---
+++ Assets/Scripts/MusicLoader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicLoader : MonoBehaviour {
+    public GameManager gameManager;
+
+    private AudioClip loadingMusic;
+    private WWW musicLoader;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Update()
+    {
+        if (loadingMusic)
+        {
+            CheckLoading();
+        }
+    }
+
+    public void LoadMusic(string path)
+    {
+        musicLoader = new WWW("file:///" + path);
+        loadingMusic = musicLoader.GetAudioClip(true);
+        this.gameObject.SetActive(false);
+        gameObject.SetActive(true);
+    }
+
+    private void CheckLoading()
+    {
+        switch (loadingMusic.loadState)
+        {
+            case AudioDataLoadState.Loaded:
+                gameManager.GetComponent<AudioSource>().clip = loadingMusic;
+                gameManager.StartGame();
+                loadingMusic = null;
+                break;
+            case AudioDataLoadState.Failed:
+                // TODO : error message
+                this.gameObject.SetActive(true);
+                break;
+            case AudioDataLoadState.Loading:
+                // TODO : loading bar
+                var rectTransform = gameObject.GetComponentInChildren<Image>().transform as RectTransform;
+                rectTransform.sizeDelta = new Vector2(musicLoader.progress, rectTransform.sizeDelta.x);
+                break;
+        }
+    }
+}
