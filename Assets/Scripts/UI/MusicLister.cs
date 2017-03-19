1e8742ac89a163b1c0edcd08a2ec38475fafd75f
quentinCo <quentin.couillard@gmail.com>
2017-03-19 14:04:11 +0100

    *add: gameManager to MusicLister

--- Assets/Scripts/UI/MusicLister.cs
+++ Assets/Scripts/UI/MusicLister.cs
@@ -8,12 +8,13 @@ public class MusicLister : MonoBehaviour {
 
     public string musicDirectoryName;
     public GameObject buttonType;
+    public GameManager gameManager;
+
+    public float posXbutton = 7.5f;
+    public float posYbutton = -28f;
 
     private string appSoundDirectory;
     private List<string> musicList = new List<string>();
-
-    private float posXbutton = 7.5f;
-    private float posYbutton = -28f;
 
 	// Use this for initialization
 	void Start () {
@@ -40,39 +41,24 @@ public class MusicLister : MonoBehaviour {
     // Genere les bouttons pour la selection
     private void GenerateButtons()
     {
-        MusicLoader musicLoader = null;// = GetComponentInChildren<MusicLoader>();
-        var trs = GetComponentsInChildren(typeof(Transform), true);
-        foreach (Transform t in trs)
-        {
-            if (t.GetComponent<MusicLoader>())
-            {
-                musicLoader = t.GetComponent<MusicLoader>();
-                break;
-            }
-        }
         var musicButtons = GameObject.Find("MusicButtonsContent");
-        /*for (int i = 0; i < 5; i++)
-        {
-            GameObject goButton = (GameObject)Instantiate(buttonType);
-            //goButton.transform.SetParent(ParentPanel, false);
-            goButton.transform.localScale = new Vector3(1, 1, 1);
-
-            Button tempButton = goButton.GetComponent<Button>();
-            int tempInt = i;
-        }*/
+        
         for (int i = 0; i < musicList.Count; ++i)
         {
+            // Crea boutton
             GameObject newObj = Instantiate(buttonType);
             newObj.transform.SetParent(musicButtons.gameObject.transform);
             newObj.transform.localPosition = new Vector3(posXbutton, posYbutton * i, 0);
+
+            // Redimensionnement du conteneur
             RectTransform obj = (RectTransform)newObj.transform;
             RectTransform content = (RectTransform)musicButtons.transform;
-
             if (Mathf.Abs(newObj.transform.localPosition.y) > content.rect.height)
                musicButtons.GetComponent<RectTransform>().sizeDelta = new Vector2(0, Mathf.Abs(content.rect.height - newObj.transform.localPosition.y));
 
+            // Initialisation des parametrer du boutton
             SelectMusicButton buttonComponent = newObj.GetComponent<SelectMusicButton>();
-            buttonComponent.SetMusicLoader(musicLoader);
+            buttonComponent.SetManager(gameManager);
             buttonComponent.SetMusicPath(appSoundDirectory + "/" + musicList[i % 2]);
             buttonComponent.SetMusicName(musicList[i % 2].Substring(0, musicList[i % 2].Length - 4)); // 4 size of ".mp3" | ".wav"
         }
