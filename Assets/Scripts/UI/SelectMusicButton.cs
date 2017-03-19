8723c9b934360485753949264b00234b9b96c54e
quentinCo <quentin.couillard@gmail.com>
2017-03-19 12:47:04 +0100

    *new: SelectMusicButton.cs

---
+++ Assets/Scripts/UI/SelectMusicButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectMusicButton : MonoBehaviour {
+
+    private string musicName = "";
+    private string musicPath = "";
+    public MusicLoader musicLoader;
+
+    void Start()
+    {
+        Button btnComponent = gameObject.GetComponent<Button>();
+        btnComponent.GetComponentInChildren<Text>().text = musicName;
+        btnComponent.onClick.AddListener(TaskOnClick);
+    }
+
+    public void TaskOnClick()
+    {
+        Debug.Assert(musicLoader);
+        musicLoader.LoadMusic(musicPath);
+    }
+
+    public void SetMusicName(string name)
+    {
+        musicName = name;
+        gameObject.GetComponent<Button>().GetComponentInChildren<Text>().text = musicName;
+    }
+
+    public void SetMusicPath(string path)
+    {
+        musicPath = path;
+    }
+
+    public void SetMusicLoader(MusicLoader gm)
+    {
+        musicLoader = gm;
+    }
+
+}
